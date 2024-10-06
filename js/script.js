e1e1098668321e7004d00720875d6316759e45f1
Sergey <sinzem@gmail.com>
2024-10-06 20:32:01 +0300

    choose

--- js/script.js
+++ js/script.js
@@ -6,9 +6,21 @@ window.addEventListener("DOMContentLoaded", () => {
     const headerMenuDropDown = document.querySelector(".header__menu_drop_down");
     const menuItems = document.querySelectorAll(".header__menu__item");
     const headerMenuButton = document.querySelector(".header__menu__button");
+    const windowWidth = window.innerWidth;
 
-    window.addEventListener("scroll", () => {
-        window.scrollY > 200 ? headerMenu.classList.add("active") : headerMenu.classList.remove("active");
+    if (windowWidth > 1199) {
+        window.addEventListener("scroll", () => {
+            window.scrollY > 200 ? headerMenu.classList.add("active") : headerMenu.classList.remove("active");
+        })
+    }
+
+    headerMenuDropDown.addEventListener("click", () => {
+        if(headerMenuDropDown.classList.contains("active")) {
+            dropMenuClose();
+        } else {
+            dropMenuOpen();
+        }
+
     })
     
     menuItems.forEach(item => {
@@ -22,39 +34,51 @@ window.addEventListener("DOMContentLoaded", () => {
 
     headerMenuButton.addEventListener("click", () => {
         if (headerMenuDrop.classList.contains("active")) {
-            headerMenuDrop.classList.remove("active");
-            headerMenuDropDown.classList.remove("active");
-            document.body.style.overflow = "";
-            document.body.style.marginRight = `0px`;
+            dropMenuClose();
         } else {
-            headerMenuDrop.classList.add("active");
-            headerMenuDropDown.classList.add("active");
-            document.body.style.overflow = "hidden";
-            document.body.style.marginRight = `${scroll}px`;
+            dropMenuOpen();
         }
     })
+
+    function dropMenuOpen() {
+        headerMenuDrop.classList.add("active");
+        headerMenuDropDown.classList.add("active");
+        document.body.style.overflow = "hidden";
+        document.body.style.marginRight = `${scroll}px`;
+    }
+
+    function dropMenuClose() {
+        headerMenuDrop.classList.remove("active");
+        headerMenuDropDown.classList.remove("active");
+        document.body.style.overflow = "";
+        document.body.style.marginRight = `0px`;
+    }
+
+
 
     // for scrolling wide blocks 
     const wideBlocks = document.querySelectorAll(".scroll_horizontal");
     const scroll = calcScroll();
 
-    wideBlocks.forEach(block => {
-        block.addEventListener("mouseenter", () => {
-            document.body.style.overflow = 'hidden';
-            document.body.style.marginRight = `${scroll}px`;
-            block.addEventListener("wheel", (e) => {
-                let position = e.deltaY;
-                block.scrollLeft += position * 1.5;
+    if (windowWidth > 1199) {
+        wideBlocks.forEach(block => {
+            block.addEventListener("mouseenter", () => {
+                document.body.style.overflow = 'hidden';
+                document.body.style.marginRight = `${scroll}px`;
+                block.addEventListener("wheel", (e) => {
+                    let position = e.deltaY;
+                    block.scrollLeft += position * 1.5;
+                })
             })
         })
-    })
-
-    wideBlocks.forEach(block => {
-        block.addEventListener("mouseleave", () => {
-            document.body.style.overflow = '';
-            document.body.style.marginRight = `0px`;
+    
+        wideBlocks.forEach(block => {
+            block.addEventListener("mouseleave", () => {
+                document.body.style.overflow = '';
+                document.body.style.marginRight = `0px`;
+            })
         })
-    })
+    }
 
     // scrollWidth
     function calcScroll() { 
@@ -71,4 +95,28 @@ window.addEventListener("DOMContentLoaded", () => {
 
         return scrollWidth; 
     }
+
+    // services cards
+    const servicesButtons = document.querySelectorAll(".services__item__button")
+    const servicesCards = document.querySelectorAll(".services__modal_item");
+
+    servicesButtons.forEach((button, i) => {
+        button.addEventListener("click", () => {
+            if (!button.classList.contains("active")) {
+                servicesButtons.forEach(item => {
+                    if (item.classList.contains("active")) {
+                        item.classList.remove("active");
+                    }
+                })
+                button.classList.add("active");
+                servicesCards.forEach((card, j) => {
+                    if (j === i) {
+                        card.classList.add("active");
+                    } else {
+                        card.classList.remove("active");
+                    }
+                })
+            }
+        })
+    })
 })
