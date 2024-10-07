ba2b7b34a5cddd273fd47078cf542e6d6eccaae8
Sergey <sinzem@gmail.com>
2024-10-08 01:35:22 +0300

    trust_plans

--- js/script.js
+++ js/script.js
@@ -22,15 +22,13 @@ window.addEventListener("DOMContentLoaded", () => {
         }
 
     })
-    
-    menuItems.forEach(item => {
-        item.addEventListener("click", () => {
-            menuItems.forEach(item => {
-                item.classList.contains("active") ? item.classList.remove("active") : null;
-            })
-            item.classList.add("active");
-        })
-    })
+
+    function dropMenuOpen() {
+        headerMenuDrop.classList.add("active");
+        headerMenuDropDown.classList.add("active");
+        document.body.style.overflow = "hidden";
+        document.body.style.marginRight = `${scroll}px`;
+    }
 
     headerMenuButton.addEventListener("click", () => {
         if (headerMenuDrop.classList.contains("active")) {
@@ -40,21 +38,21 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     })
 
-    function dropMenuOpen() {
-        headerMenuDrop.classList.add("active");
-        headerMenuDropDown.classList.add("active");
-        document.body.style.overflow = "hidden";
-        document.body.style.marginRight = `${scroll}px`;
-    }
-
     function dropMenuClose() {
         headerMenuDrop.classList.remove("active");
         headerMenuDropDown.classList.remove("active");
         document.body.style.overflow = "";
         document.body.style.marginRight = `0px`;
     }
-
-
+    
+    menuItems.forEach(item => {
+        item.addEventListener("click", () => {
+            menuItems.forEach(item => {
+                item.classList.contains("active") ? item.classList.remove("active") : null;
+            })
+            item.classList.add("active");
+        })
+    })
 
     // for scrolling wide blocks 
     const wideBlocks = document.querySelectorAll(".scroll_horizontal");
@@ -80,22 +78,6 @@ window.addEventListener("DOMContentLoaded", () => {
         })
     }
 
-    // scrollWidth
-    function calcScroll() { 
-        let div = document.createElement('div'); 
-
-        div.style.width = '50px'; 
-        div.style.height = '50px';
-        div.style.overflowY = 'scroll'; 
-        div.style.visibility = 'hidden'; 
-
-        document.body.appendChild(div); 
-        let scrollWidth = div.offsetWidth - div.clientWidth; 
-        div.remove(); 
-
-        return scrollWidth; 
-    }
-
     // services cards
     const servicesButtons = document.querySelectorAll(".services__item__button")
     const servicesCards = document.querySelectorAll(".services__modal_item");
@@ -119,4 +101,52 @@ window.addEventListener("DOMContentLoaded", () => {
             }
         })
     })
+
+
+    // portfolio filter
+    const portfolioFilters = document.querySelector(".portfolio__filters");
+    const portfolioButtons = document.querySelectorAll(".portfolio__button");
+    const portfolioCase = document.querySelectorAll(".portfolio__case");
+
+    portfolioFilters.addEventListener("click", (e) => {
+        let btn = e.target;
+
+        if (btn.tagName != "BUTTON") return;
+
+        if (!btn.classList.contains("active")) {
+            let filter = btn.dataset.filter;
+            portfolioButtons.forEach(item => {
+                if (item.classList.contains("active")) item.classList.remove("active");
+            })
+            btn.classList.add("active");
+            portfolioCase.forEach(card => {
+                if (btn.dataset.filter === "all") {
+                    card.classList.add("active");
+                } else {
+                    card.classList.contains(`portfolio__case_${filter}`) ?
+                        card.classList.add("active") : 
+                        card.classList.remove("active");
+
+                }
+            })
+        }
+    })
 })
+
+
+
+// scrollWidth
+function calcScroll() { 
+    let div = document.createElement('div'); 
+
+    div.style.width = '50px'; 
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll'; 
+    div.style.visibility = 'hidden'; 
+
+    document.body.appendChild(div); 
+    let scrollWidth = div.offsetWidth - div.clientWidth; 
+    div.remove(); 
+
+    return scrollWidth; 
+}
